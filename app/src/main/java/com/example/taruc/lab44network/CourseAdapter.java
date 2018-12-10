b34e466730f03522e4e6252cafdadae219c7f6a6
Henry <dingyh-wa16@student.tarc.edu.my>
2018-12-10 10:03:10 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/taruc/lab44network/CourseAdapter.java
@@ -0,0 +1,43 @@
+package com.example.taruc.lab44network;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by TARC on 8/6/2015.
+ */
+public class CourseAdapter extends ArrayAdapter<Course> {
+
+    public CourseAdapter(Activity context, int resource, List<Course> list) {
+        super(context, resource, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //Retrieve a record from list
+        Course course = getItem(position);
+        //Create an instance for inflator
+        LayoutInflater inflater  = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //Show a layout to display a record
+        View rowView = inflater.inflate(R.layout.course_record, parent, false);
+
+        TextView textViewCode, textViewTitle, textViewCredit;
+
+        textViewCode = rowView.findViewById(R.id.textViewCode);
+        textViewTitle = rowView.findViewById(R.id.textViewTitle);
+        textViewCredit = rowView.findViewById(R.id.textViewCredit);
+
+        textViewCode.setText(String.format("%s : %s",getContext().getString(R.string.code),course.getCode()));
+        textViewTitle.setText(String.format("%s : %s",getContext().getString(R.string.title), course.getTitle()));
+        textViewCredit.setText(String.format("%s : %s",getContext().getString(R.string.credit),course.getCredit()));
+        return rowView;
+    }
+}
